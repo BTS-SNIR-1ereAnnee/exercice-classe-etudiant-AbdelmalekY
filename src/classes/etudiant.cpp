6f6ee4b868072c2abc6222fb2c37fc9270420f77
eleve <abdelmalek.younes@lyceeppr.fr>
2020-03-26 12:46:19 +0100

    ajout de etudiant.cpp et commencement du codage

---
+++ src/classes/etudiant.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <etudiant.h>
+
+using namespace std;
---
+++ src/classes/etudiant.h
@@ -0,0 +1,23 @@
+#ifndef ETUDIANT_H
+#define ETUDIANT_H
+
+class Etudiant
+{
+public:
+	Etudiant(char nom,int note1,int note2);
+
+	void setEtudiant(char nom,int note1,int note2)
+
+private:
+	char m_nom;
+	int m_note1,m_note2;
+
+
+
+
+
+
+
+
+
+}
